7f701ed9a7a2b9b4ca3c7ce70e924d78463c5184
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-05-19 11:17:37 +0530

    simple login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  let [username, setUsername] = useState('')
+  let [password, setPassword] = useState('')
+
+  let handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(username, password);
+  }
+
+  // let getUsername = (event) => {
+  //   setUsername(event.target.value)
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-lg-6'>
+            <form onSubmit={handleSubmit}>
+              <div className='text-start my-3'>
+                <label>Username: </label>
+                <input type='text' onChange={(event) => setUsername(event.target.value)} className='form-control' value={username} />
+              </div>
+              <div className='text-start my-3'>
+                <label>Password: </label>
+                <input type='password' onChange={(event) => setPassword(event.target.value)} className='form-control' value={password} />
+              </div>
+              <div className='text-start my-3'>
+                <button>Login</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
